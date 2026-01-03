eb8fe4d6da7c1f06ca4d6ba74b0b12e94c1f4254
Moustosidad <alonso.alonsodeleonruiz@gmail.com>
2026-01-03 22:13:48 +0100

    Implement MainMenu for no command-line arguments
    Added a check for command-line arguments to run the MainMenu if no arguments are provided.

--- aegispassgen/cli.py
+++ aegispassgen/cli.py
@@ -1,13 +1,19 @@
+import sys
 from aegispassgen.config import Config
 from aegispassgen.core.engine import AegisEngine
-from aegispassgen.ui.tui import AegisTUI
+from aegispassgen.ui.menu import MainMenu
 
 
 def main():
+    if len(sys.argv) == 1:
+        MainMenu().run()
+        return
+
     config = Config.from_args()
     engine = AegisEngine(config)
 
     if config.ui:
+        from aegispassgen.ui.tui import AegisTUI
         AegisTUI(engine).run()
     else:
         for pwd in engine.generate():
